bf64e93e379af9d17b6f633c3fc39965ead7cd92
박혜민 <hyemin@baghyemin-ui-MacBookPro.local>
2022-12-06 09:47:22 +0900

    counter-redux-test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+  // if (action.type === "ADD") {
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+};
+
+const countStore = createStore(countModifier); // 데이터 저장하는 data 의 store를 만들고
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange); //subscribe 변화 감지할때 마다 onChange 실행
+
+// countStore.dispatch({ type: "ADD" });
+// console.log(countStore.getState());
+add.addEventListener("click", () => {
+  countStore.dispatch({ type: ADD });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: MINUS });
+});
